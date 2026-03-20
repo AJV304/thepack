7222d74774508119ba43c7a68e367a8783ebaf8c
Hanne Oberman <h.i.oberman@uu.nl>
2026-03-20 14:13:51 +0100

    suggestions for reusability

--- R/analysis.R
+++ R/analysis.R
@@ -6,34 +6,24 @@
 #' @param b1 Main effect coefficient in the effect scenario
 #' @param b_z Continuous covariate effect coefficient
 #' @param b_d Dichotomous covariate effect coefficient
-#' @param dep The dependent variable ("y_yes" or "y_no")
+#' @param it The iteration number
 #' @return b1, p-value and confidence interval for baseline and each deviation
 #'   scenario
 #' @export
 
-analysis <- function(iter, n, b0, b1, b_z, b_d, dep) {
+analysis <- function(iter, n, b0, b1, b_z, b_d, it) {
+  #simulate a dataset
+  df <- dgm(n, b0, b1, b_z, b_d)
 
-  #create empty dataframe for the statistics
-  stats <- data.frame(matrix(ncol = 5, nrow = (10*iter)))
-  colnames(stats) <- c("b1","p.value","lower.ci","upper.ci", "condition")
+  #run all scenarios on the dataset
+  result_yes <- conditions(df, "y_yes") |>
+    stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
+    cbind(scenario = "effect", iteration = it)
+  result_no <- conditions(df, "y_no")  |>
+    stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
+    cbind(scenario = "effect", iteration = it)
 
-  for (i in 1:iter) {
+  results <- rbind(result_yes, result_no)
 
-    #set.seed(i)
-
-    #simulate a dataset
-    df <- dgm(n, b0, b1, b_z, b_d)
-
-    #run all scenarios on the dataset
-    result <- conditions(df, dep)
-
-    #add scenarios to the stats dataset and add iteration number
-    stats[(((i-1)*10+1)):(i*10),] <- result
-    stats$iteration[(((i-1)*10+1)):(i*10)] <- i
-
-  }
-
-  return(stats)
-  }
-
-
+  return(results)
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This repository is for the R package 'thepack', which includes all the necessary functions to produce the result of my thesis work. 
+
+[how to use this package? which function to call to run the simulation study?]
