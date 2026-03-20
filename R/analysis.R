b0568b656497dd2257dac43f9203ef6e1e90486b
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-03-20 15:07:23 +0100

    changing dgm to not need 'dep'

--- R/analysis.R
+++ R/analysis.R
@@ -6,22 +6,28 @@
 #' @param b1 Main effect coefficient in the effect scenario
 #' @param b_z Continuous covariate effect coefficient
 #' @param b_d Dichotomous covariate effect coefficient
-#' @param it The iteration number
 #' @return b1, p-value and confidence interval for baseline and each deviation
 #'   scenario
 #' @export
-analysis <- function(iter, n, b0, b1, b_z, b_d, dep) {
-  #simulate a dataset
+analysis <- function(iter, n, b0, b1, b_z, b_d) {
+
+  for (i in 1:iter) {
+
+   #simulate a dataset
   df <- dgm(n, b0, b1, b_z, b_d)
+
   #run all scenarios on the dataset
   result_yes <- conditions(df, "y_yes") |>
     stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
-    cbind(scenario = "effect", iteration = it)
+    cbind(scenario = "effect", iteration = i)
+
   result_no <- conditions(df, "y_no")  |>
     stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
-    cbind(scenario = "effect", iteration = it)
+    cbind(scenario = "no effect", iteration = i)
+
   # combine results
-  results <- rbind(result_yes, result_no)
+  results[(((i-1)*20+1)):(i*20),] <- rbind(result_yes, result_no)
+  }
 
   return(results)
 }
