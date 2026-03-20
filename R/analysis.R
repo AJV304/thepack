91e2b2062b1334e8a86df7cc0a6247b8efddb109
Hanne Oberman <h.i.oberman@uu.nl>
2026-03-20 14:14:24 +0100

    Revert "suggestions for reusability"
    This reverts commit 7222d74774508119ba43c7a68e367a8783ebaf8c.

--- R/analysis.R
+++ R/analysis.R
@@ -6,24 +6,34 @@
 #' @param b1 Main effect coefficient in the effect scenario
 #' @param b_z Continuous covariate effect coefficient
 #' @param b_d Dichotomous covariate effect coefficient
-#' @param it The iteration number
+#' @param dep The dependent variable ("y_yes" or "y_no")
 #' @return b1, p-value and confidence interval for baseline and each deviation
 #'   scenario
 #' @export
 
-analysis <- function(iter, n, b0, b1, b_z, b_d, it) {
-  #simulate a dataset
-  df <- dgm(n, b0, b1, b_z, b_d)
+analysis <- function(iter, n, b0, b1, b_z, b_d, dep) {
 
-  #run all scenarios on the dataset
-  result_yes <- conditions(df, "y_yes") |>
-    stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
-    cbind(scenario = "effect", iteration = it)
-  result_no <- conditions(df, "y_no")  |>
-    stats::setNames(c("b1", "p.value", "lower.ci", "upper.ci", "condition")) |>
-    cbind(scenario = "effect", iteration = it)
+  #create empty dataframe for the statistics
+  stats <- data.frame(matrix(ncol = 5, nrow = (10*iter)))
+  colnames(stats) <- c("b1","p.value","lower.ci","upper.ci", "condition")
 
-  results <- rbind(result_yes, result_no)
+  for (i in 1:iter) {
 
-  return(results)
-}
+    #set.seed(i)
+
+    #simulate a dataset
+    df <- dgm(n, b0, b1, b_z, b_d)
+
+    #run all scenarios on the dataset
+    result <- conditions(df, dep)
+
+    #add scenarios to the stats dataset and add iteration number
+    stats[(((i-1)*10+1)):(i*10),] <- result
+    stats$iteration[(((i-1)*10+1)):(i*10)] <- i
+
+  }
+
+  return(stats)
+  }
+
+
